9aff40dc7a5ee45c36d453c29a36c7ea3f06c7d3
tmich20 <140355225+tmich20@users.noreply.github.com>
2023-07-27 12:50:49 +1000

    Update streamlit_app.py
    final commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ try:
 except URLError as e:
   streamlit.error()
   
-streamlit.header("The fruit laod list contains")
+streamlit.header("View Our Fruit List - Ass Your Favourites!")
 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
